3cabd2d0c0853476bd961dd49bf1d5d8937357af
jonas <jonas.kowalick@gmail.com>
2017-12-06 21:00:52 -0800

    added inputs to download events in a specific time period

--- content.js
+++ content.js
@@ -1,36 +1,38 @@
 (() => {
   const SCRIPT_ID = 'CDCalExport';
+  const START_DATE_INPUT = 'Start Date';
+  const END_DATE_INPUT = 'End Date';
 
   // sets the data attributes of the script tag so we can use the data in the content script
   // user and hash are global variables on the site ¯\_(ツ)_/¯
-  function setDataAttributes() {
+  const setDataAttributes = () => {
     // eslint-disable-next-line no-undef
     document.currentScript.setAttribute('data-user', user);
     // eslint-disable-next-line no-undef
     document.currentScript.setAttribute('data-hash', hash);
-  }
+  };
 
 
   // inject script into the page to retrieve the global variables user and hash
-  function injectScript(code) {
+  const injectScript = (code) => {
     const script = document.createElement('script');
     script.setAttribute('id', SCRIPT_ID);
     script.appendChild(document.createTextNode(`( ${code} )();`));
     (document.body || document.head || document.documentElement).appendChild(script);
 
     return Promise.resolve();
-  }
+  };
 
 
-  function getDataAttribute(attributeName) {
+  const getDataAttribute = (attributeName) => {
     const scriptNode = document.querySelector(`#${SCRIPT_ID}`);
     const attributeValue = scriptNode.getAttribute(`data-${attributeName}`);
 
     return attributeValue;
-  }
+  };
 
 
-  function formatDateTime(date, utc) {
+  const formatDateTime = (date, utc) => {
     const jsDate = typeof date === 'object' ? date : new Date(date * 1000);
     const leftpad = str => (`00${str}`).substring(str.length);
 
@@ -47,10 +49,10 @@
     const datestring = `${year}${month}${day}T${hour}${minute}${second}${utc ? 'Z' : ''}`;
 
     return datestring;
-  }
+  };
 
 
-  function createDownloadButton(text, buttonFunction) {
+  const createDownloadButton = (text, buttonFunction) => {
     const button = document.createElement('a');
     button.setAttribute('class', SCRIPT_ID);
     button.download = 'BA-Kalender.ics';
@@ -58,35 +60,58 @@
     button.onclick = buttonFunction;
 
     return button;
-  }
+  };
+
+  const createDateInput = (labelText) => {
+    const id = labelText.split(' ').join('');
+    const input = document.createElement('input');
+    input.type = 'date';
+    input.id = id;
+    input.valueAsDate = new Date();
+
+    const label = createDateLabel(id);
+
+    const container = document.createElement('div');
+    container.appendChild(label);
+    container.appendChild(input);
+    return container;
+  };
+
+  const createDateLabel = (id, labelText) => {
+    const label = document.createElement('label');
+    label.textContent = labelText;
+    label.setAttribute('for', id);
+
+    return label;
+  };
 
 
-  function getEvents(user, hash, start, end) {
+  const getEvents = (user, hash, start, end) => {
     const base = 'https://selfservice.campus-dual.de/room/json';
 
     const url = `${base}?userid=${user}&hash=${hash}&start=${start}&end=${end}`;
 
     return fetch(url).then(success => success.json());
-  }
+  };
 
 
-  function buildICalEvent(event) {
-    const iCalEvent =
-    `BEGIN:VEVENT
-DTSTAMP:${formatDateTime(new Date(), true)}
-UID:${event.title}${event.start + event.end}
-SUMMARY:${event.title} | ${event.room} | ${event.instructor}
-LOCATION:${event.room}
-DTSTART:${formatDateTime(event.start, false)}
-DTEND:${formatDateTime(event.end, false)}
-END:VEVENT
-`;
+  const buildICalEvent = (event) => {
+    const iCalEvent = [
+        `BEGIN:VEVENT`,
+        `DTSTAMP:${formatDateTime(new Date(), true)}`,
+        `UID:${event.title}${event.start + event.end}`,
+        `SUMMARY:${event.title} | ${event.room} | ${event.instructor}`,
+        `LOCATION:${event.room}`,
+        `DTSTART:${formatDateTime(event.start, false)}`,
+        `DTEND:${formatDateTime(event.end, false)}`,
+        `END:VEVENT\n`,
+    ].join('\n');
 
     return iCalEvent;
-  }
+  };
 
 
-  function generateCalString(events) {
+  const generateCalString = (events) => {
     const start = 'BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:Campus-Dual-Calendar-Export\nX-WR-CALNAME:BA-Dresden\nX-WR-TIMEZONE:Europe/Berlin\n';
     const end = 'END:VCALENDAR';
 
@@ -96,19 +121,42 @@ END:VEVENT
     const calstring = start + eventstring + end;
 
     return calstring;
-  }
+  };
 
 
-  injectScript(setDataAttributes).then(() => {
-    const button = createDownloadButton('loading', () => {});
-    const response = getEvents(getDataAttribute('user'), getDataAttribute('hash'), 1490487019, 1490488000);
-    response.then(events => generateCalString(events)).then((calstring) => {
+  const setDownloadLink = (clickEvent) => {
+    const button = clickEvent.target;
+    button.text = 'loading...';
+
+    startDateInput = document.getElementById(START_DATE_INPUT.split(' ').join(''));
+    endDateInput = document.getElementById(END_DATE_INPUT.split(' ').join(''));
+
+    getEvents(
+      getDataAttribute('user'),
+      getDataAttribute('hash'),
+      Math.round( startDateInput.valueAsNumber / 1000 ),
+      Math.round( endDateInput.valueAsNumber / 1000 ),
+    ).then(events => generateCalString(events)).then((calstring) => {
       const calblob = new Blob([calstring], { type: 'text/calendar;charset=UTF-8' });
       const url = window.URL.createObjectURL(calblob);
-      button.href = url;
-    }).then(() => { button.textContent = 'download .ics'; });
+      location.href = url;
+      button.text = 'Done. Click if you want to do it again.';
+    });
+  };
 
-    const calendar = document.getElementById('calendar');
+
+    /// Main
+
+  const calendar = document.getElementById('calendar');
+
+  injectScript(setDataAttributes).then(() => {
+    const button = createDownloadButton('download .ics', setDownloadLink);
+    const startDateInput = createDateInput(START_DATE_INPUT);
+    const endDateInput = createDateInput(END_DATE_INPUT);
+
+    calendar.appendChild(startDateInput);
+    calendar.appendChild(endDateInput);
+
     calendar.appendChild(button);
   });
 })();
